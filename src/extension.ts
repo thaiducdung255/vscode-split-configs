b45177a4decd2caa4da1119111ae907211714159
DungTD4 <mducdung255@gmail.com>
2025-03-02 18:27:49 +0700

    feat: add excluded file names

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,48 @@
-import * as vscode from "vscode";
-import { joinConfigs, formatPath } from "./join-configs";
+import * as vscode from 'vscode'
+import { formatPath, joinConfigs } from './join-configs'
 
 export function activate(context: vscode.ExtensionContext) {
-	const config = vscode.workspace.getConfiguration("split-configs");
-	const keybindingsPath = formatPath(config.get<string>("keybindingsPath"));
-	const settingsPath = formatPath(config.get<string>("settingsPath"));
+  const config = vscode.workspace.getConfiguration('split-configs')
+  const keybindingsPath = formatPath(config.get<string>('keybindingsPath'))
+  const settingsPath = formatPath(config.get<string>('settingsPath'))
 
-	const keybindingsPartialPath = formatPath(
-		config.get<string>("keybindingsPartialPath"),
-	);
+  const excludedFilenames = config.get<string[]>('excludedFileNames')
 
-	const settingsPartialPath = formatPath(
-		config.get<string>("settingsPartialPath"),
-	);
+  const keybindingsPartialPath = formatPath(
+    config.get<string>('keybindingsPartialPath'),
+  )
 
-	const disposable = vscode.commands.registerCommand(
-		"split-configs.join",
-		() => {
-			joinConfigs({
-			  keybindingsPath,
-			  settingsPath,
-			  settingsPartialPath,
-			  keybindingsPartialPath
-			});
-			vscode.window.showInformationMessage("Configs joined!");
-		},
-	);
+  const settingsPartialPath = formatPath(
+    config.get<string>('settingsPartialPath'),
+  )
 
-	context.subscriptions.push(disposable);
+  vscode.window.showInformationMessage(
+    `setting partial path: ${settingsPartialPath}`,
+  )
+
+  vscode.window.showInformationMessage(
+    `key binding partial path: ${keybindingsPartialPath}`,
+  )
+
+  vscode.window.showInformationMessage(
+    `excluded file names: ${(excludedFilenames || []).join(', ')}`,
+  )
+
+  const disposable = vscode.commands.registerCommand(
+    'split-configs.join',
+    () => {
+      joinConfigs({
+        keybindingsPath,
+        settingsPath,
+        settingsPartialPath,
+        keybindingsPartialPath,
+        excludedFilenames,
+      })
+      vscode.window.showInformationMessage('Configs joined!')
+    },
+  )
+
+  context.subscriptions.push(disposable)
 }
 
 export function deactivate() {}
--- src/join-configs.ts
+++ src/join-configs.ts
@@ -1,123 +1,128 @@
-const { writeFileSync, readdirSync } = require("node:fs");
+const { writeFileSync, readdirSync } = require('node:fs')
 
 function getPartialConfig(
-	dir: string,
-	isArray = true,
-	fts = ["json"],
-	excludedFilenames = ["package.json", "package-lock.json"],
+  dir: string,
+  isArray = true,
+  fts = ['json'],
+  excludedFilenames = ['package.json', 'package-lock.json'],
 ) {
-	console.log(
-		`Getting partial configs from: ${dir}, filetypes: ${fts.join(", ")}, excluded filenames: ${excludedFilenames.join(", ")}`,
-	);
+  process.stdout.write(
+    `Getting partial configs from: ${dir}, filetypes: ${fts.join(', ')}, excluded filenames: ${excludedFilenames.join(', ')}\n`,
+  )
 
-	let configs: KeyBinding[] | object = isArray ? [] : {};
+  let configs: KeyBinding[] | object = isArray ? [] : {}
 
-	const filenames = readdirSync(dir).filter((filename: string) => {
-		const ft = filename.split(".").slice(-1)[0];
-		return !excludedFilenames.includes(filename) && fts.includes(ft);
-	});
+  const filenames = readdirSync(dir).filter((filename: string) => {
+    const ft = filename.split('.').slice(-1)[0]
+    return !excludedFilenames.includes(filename) && fts.includes(ft)
+  })
 
-	console.log(`Found ${filenames.length} valid files: ${filenames.join(", ")}`);
+  process.stdout.write(
+    `Found ${filenames.length} valid files: ${filenames.join(', ')}\n`,
+  )
 
-	for (const filename of filenames) {
-		const partialConfigs = require(`${dir}/${filename}`);
+  for (const filename of filenames) {
+    const partialConfigs = require(`${dir}/${filename}`)
 
-		if (Array.isArray(configs)) {
-			configs.push(...partialConfigs);
-		} else {
-			configs = { ...configs, ...partialConfigs };
-		}
-	}
+    if (Array.isArray(configs)) {
+      configs.push(...partialConfigs)
+    } else {
+      configs = { ...configs, ...partialConfigs }
+    }
+  }
 
-	console.log("Got partial config files done");
-	return configs;
+  process.stdout.write('Got partial config files done\n')
+  return configs
 }
 
 type KeyBinding = {
-	key: string;
-	command: string;
-	when: string;
-};
+  key: string
+  command: string
+  when: string
+}
 
 function removeDuplicatedConfigs(configs: KeyBinding[]) {
-	console.log("Removing dupplicated configs");
-	const originalConfigCount = configs.length;
-	let removeCount = 0;
+  process.stdout.write('Removing dupplicated configs\n')
+  const originalConfigCount = configs.length
+  let removeCount = 0
 
-	for (let i = 0; i < configs.length; i++) {
-		const { key, command, when } = configs[i];
+  for (let i = 0; i < configs.length; i++) {
+    const { key, command, when } = configs[i]
 
-		for (const oldConfig of configs.slice(0, i)) {
-			const { key: _key, command: _command, when: _when } = oldConfig;
+    for (const oldConfig of configs.slice(0, i)) {
+      const { key: _key, command: _command, when: _when } = oldConfig
 
-			if (key === _key && command === _command && when === _when) {
-				console.log("Dupplicated config:", command, when);
-				configs.splice(i, 1);
-				i--;
-				removeCount++;
-				break;
-			}
-		}
-	}
+      if (key === _key && command === _command && when === _when) {
+        process.stdout.write(`Dupplicated config: ${command} ${when}\n`)
+        configs.splice(i, 1)
+        i--
+        removeCount++
+        break
+      }
+    }
+  }
 
-	console.log(`Removed ${removeCount}/${originalConfigCount} configs`);
+  process.stdout.write(
+    `Removed ${removeCount}/${originalConfigCount} configs\n`,
+  )
 }
 
 function gatherVsCodeConfig(opts: {
-	configPath: string;
-	partialDir: string;
-	isArray?: boolean;
-	fts?: string[];
-	excludedFilenames?: string[];
+  configPath: string
+  partialDir: string
+  isArray?: boolean
+  fts?: string[]
+  excludedFilenames?: string[]
 }) {
-	const { configPath, partialDir, isArray, fts, excludedFilenames } = opts;
-	const configs = getPartialConfig(partialDir, isArray, fts, excludedFilenames);
-	isArray && Array.isArray(configs) && removeDuplicatedConfigs(configs);
-	writeFileSync(configPath, JSON.stringify(configs, null, "\t"));
+  const { configPath, partialDir, isArray, fts, excludedFilenames } = opts
+  const configs = getPartialConfig(partialDir, isArray, fts, excludedFilenames)
+  isArray && Array.isArray(configs) && removeDuplicatedConfigs(configs)
+  writeFileSync(configPath, JSON.stringify(configs, null, '\t'))
 
-	console.log(
-		`Wrote ${Array.isArray(configs) ? configs.length : 1} configs to ${configPath}`,
-	);
+  process.stdout.write(
+    `Wrote ${Array.isArray(configs) ? configs.length : 1} configs to ${configPath}\n`,
+  )
 }
 
 export function formatPath(rawPath: string | undefined) {
-	const { HOME } = process.env;
+  const { HOME } = process.env
 
-	if (!rawPath) {
-		throw new Error("Path not found");
-	}
+  if (!rawPath) {
+    throw new Error('Path not found')
+  }
 
-	if (!HOME) {
-		throw new Error("HOME environment variable not found");
-	}
+  if (!HOME) {
+    throw new Error('HOME environment variable not found')
+  }
 
-	return rawPath.replace(/^~/, HOME);
+  return rawPath.replace(/^~/, HOME)
 }
 
 export function joinConfigs(input: {
-	keybindingsPath: string;
-	settingsPath: string;
-	keybindingsPartialPath: string;
-	settingsPartialPath: string;
-	excludedFilenames?: string[];
+  keybindingsPath: string
+  settingsPath: string
+  keybindingsPartialPath: string
+  settingsPartialPath: string
+  excludedFilenames?: string[]
 }) {
-	const {
-		keybindingsPath,
-		keybindingsPartialPath,
-		settingsPath,
-		settingsPartialPath,
-		excludedFilenames,
-	} = input;
-	gatherVsCodeConfig({
-		configPath: keybindingsPath,
-		partialDir: keybindingsPartialPath,
-		excludedFilenames,
-	});
+  const {
+    keybindingsPath,
+    keybindingsPartialPath,
+    settingsPath,
+    settingsPartialPath,
+    excludedFilenames,
+  } = input
 
-	gatherVsCodeConfig({
-		configPath: settingsPath,
-		partialDir: settingsPartialPath,
-		isArray: false,
-		excludedFilenames,
-	});
+  gatherVsCodeConfig({
+    configPath: keybindingsPath,
+    partialDir: keybindingsPartialPath,
+    excludedFilenames,
+  })
+
+  gatherVsCodeConfig({
+    configPath: settingsPath,
+    partialDir: settingsPartialPath,
+    isArray: false,
+    excludedFilenames,
+  })
 }
